350d49b9a8c522337f2da701601b599ae5626e4d
Hiya-2006 <74595695+Hiya-2006@users.noreply.github.com>
2020-12-31 08:45:30 +0530

    Add files via upload

---
+++ box1.js
@@ -0,0 +1,29 @@
+class box{
+    constructor(x,y,width,height){
+        var options ={
+            restitution: 0.7,
+            friction:1.0,
+            isStatic: false
+            
+        }
+        
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width=width
+        this.height=height
+
+        World.add(world,this.body);
+    }
+
+      display(){
+
+          var angle=this.body.angle
+          var pos=this.body.position
+          push ()
+          translate(pos.x,pos.y)
+          rotate (angle)
+        rectMode(CENTER);
+    fill("red")
+    rect(0,0,this.width, this.height);
+    pop ()
+        }
+}
